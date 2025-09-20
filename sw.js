d12a77661946d296ab297281187a70e3d776a76c
gd12162 <greatgd93@gmail.com>
2025-09-20 09:16:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
-// sw.js v7 code here
+// Offline cache for app shell (GitHub Pages friendly)
+const CACHE = '100days-v6';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k!==CACHE)?caches.delete(k):null)))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
